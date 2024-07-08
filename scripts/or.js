53fdf82d291a35833fda773ce129bc2c697a27e1
Your Name <youremail@yourdomain.com>
2024-07-08 11:30:06 +0100

    Corrected image choices. Added makeshift 18+ content warning for H/S Link

---
+++ scripts/or.js
@@ -0,0 +1,7 @@
+var elems = document.getElementsByClassName('a6');
+var confirmIt = function (e) {
+if (!confirm('Please be aware that by clicking "okay", you confirm that you are aged 18+')) e.preventDefault();
+};
+for (var i = 0, l = elems.length; i < l; i++) {
+elems[i].addEventListener('click', confirmIt, false);
+}
