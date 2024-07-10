dbc5194223490a68c989fb4909632798f0cca3f7
Your Name <youremail@yourdomain.com>
2024-07-10 09:20:31 +0100

    Was unable to achieve desired pop up effects. Will need to create more pages for anime choices. Colour theme still not finalized.

---
+++ scripts/main-cu.js
@@ -0,0 +1,39 @@
+function validate() { 
+let fname = 
+    document.getElementById("fname").value;
+let sname = 
+    document.getElementById("sname").value; 
+let subject =
+    document.getElementById("subject")
+let email = 
+    document.getElementById("emadd").value; 
+let message = 
+    document.getElementById("message").value; 
+let error_message = 
+    document.getElementById("error_message"); 
+  
+error_message.style.padding = "10px"; 
+  
+let errors = [];
+
+if (fname.length <= 3) { //error message if your first name contains 3 or less characters//
+    errors.push("Please Enter a valid Name");}
+if (sname.length <= 3) { //error message if your surname contains 3 or less characters//
+    errors.push("Please Enter a valid Surname");}
+if (subject.length < 1) { //error message if subject character count is below one.
+    errors.push("Please Enter a Subject");} 
+if (email.indexOf("@") == -1 || email.length <= 0) { //error if email doesn't have an @
+    errors.push( 
+        "Please Enter a valid Email Address");} 
+if (message.length <= 30) { //error if your message contains 30 or less characters total//
+    errors.push( 
+        "Please Enter More Than 30 Characters");} 
+
+if (errors.length > 0) { 
+    error_message.innerHTML = 
+        errors.join("<br>"); 
+    return false;} 
+else { 
+    alert( 
+        "Form successfully sent!"); 
+    return true;}}
