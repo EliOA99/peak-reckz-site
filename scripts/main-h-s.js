890add47c528774ce583490540049517d66e3793
Your Name <youremail@yourdomain.com>
2024-07-11 12:20:17 +0100

    18+ Warning added to relevant genre button choice -on all pages Breakthrough made on article-information issue Further got rid of/shortened unnecessary code

--- scripts/main-h-s.js
+++ scripts/main-h-s.js
@@ -14,11 +14,3 @@ document.getElementById("myInfo3").classList.toggle("infoHide3");
 function myFunction4() {
 document.getElementById("myInfo4").classList.toggle("infoHide4");  
 }
-
-var elems = document.getElementsByClassName('horrs');
-var confirmIt = function (e) {
-if (!confirm('Please be aware that by clicking "okay", you confirm that you are aged 18+')) e.preventDefault();
-};
-for (var i = 0, l = elems.length; i < l; i++) {
-elems[i].addEventListener('click', confirmIt, false);
-}
--- scripts/main-or.js
+++ scripts/main-or.js
@@ -5,3 +5,11 @@ if (!confirm('Please be aware that by clicking "okay", you confirm that you are 
 for (var i = 0, l = elems.length; i < l; i++) {
 elems[i].addEventListener('click', confirmIt, false);
 }
+
+var elems = document.getElementsByClassName('horrs');
+var confirmIt = function (e) {
+if (!confirm('Please be aware that by clicking "okay", you confirm that you are aged 18+')) e.preventDefault();
+};
+for (var i = 0, l = elems.length; i < l; i++) {
+elems[i].addEventListener('click', confirmIt, false);
+}
