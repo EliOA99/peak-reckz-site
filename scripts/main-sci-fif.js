8b724aeb1647632b3e6a9ea905e8b356803bf92c
Your Name <youremail@yourdomain.com>
2024-07-20 09:41:20 +0100

    Title info applied to all pages Javascript (read more) info element applied to all Home and About Me pages need completing Overall colour scheme needs finalizing

--- scripts/main-sci-fif.js
+++ scripts/main-sci-fif.js
@@ -15,6 +15,72 @@ function myFunction4() {
 document.getElementById("myInfo4").classList.toggle("infoHide4");  
 }
 
+
+function myFunctiona() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("expand1");
+  var btnText = document.getElementById("rmBtn1");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunctionb() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("expand2");
+  var btnText = document.getElementById("rmBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunctionc() {
+  var dots = document.getElementById("dots3");
+  var moreText = document.getElementById("expand3");
+  var btnText = document.getElementById("rmBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunctiond() {
+  var dots = document.getElementById("dots4");
+  var moreText = document.getElementById("expand4");
+  var btnText = document.getElementById("rmBtn4");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+
 var elems = document.getElementsByClassName('horrs');
 var confirmIt = function (e) {
 if (!confirm('Please be aware that by clicking "okay", you confirm that you are aged 18+')) e.preventDefault();
