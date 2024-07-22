78b098bf74a3a0e9c5db9b1236428f695d38c9d2
Your Name <youremail@yourdomain.com>
2024-07-22 12:25:28 +0100

    Homepage content complete About Us content complete May add more content to Contact Us Added Javascript back to top function Added videos Changed footer element, scrolling lessened

--- scripts/main-or.js
+++ scripts/main-or.js
@@ -15,3 +15,23 @@ if (!confirm('Please be aware that by clicking "okay", you confirm that you are 
 for (var i = 0, l = elems.length; i < l; i++) {
 elems[i].addEventListener('click', confirmIt, false);
 }
+
+//Back to top button Javascript
+let mybutton = document.getElementById("BTTBtn");
+
+// Triggers when the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrtopFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
